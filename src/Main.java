3d9d73fd4820024e3474f5b2a9585f6b0e861ac0
Your Name <your.email@example.com>
2024-11-15 19:34:03 +0400

    bolo

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int[][] square = {
-                {1, 2, 3, 4},
-                {1, 2, 3, 4},
-                {1, 2, 3, 4},
-                {1, 2, 3, 4},
+                {7, 4, 1,},
+                {8, 5, 2,},
+                {9, 6, 3,},
         };
-        int gamokleba = 0;
-        int jami = 0;
-        boolean mzadaa = false;
-        for(int i = square.length-1; i >= 0; i--){
-            if(i==0){
-                i = square.length-1;
-                jami =+ square[i][square.length-gamokleba];
-            }
-            if(gamokleba==((square.length)/2)){
-                if (mzadaa == true){
-                    System.out.println(jami);
+        int dummy= 3;
+            for(int i = 0; i <= 3; i++){
+                if(i == 3){
+                    i = 0;
+                    dummy =-1;
+                }
+                if(dummy == -1){
                     break;
                 }
-                jami =+ i-gamokleba;
-                gamokleba = 0;
-                mzadaa = true;
-            }
-            jami =+ square[i][square.length-gamokleba];
-            jami =+ square[i][square.length+gamokleba];
-
+                square[i][dummy] = square[dummy][i];
+        }
         }
     }
-}
+
