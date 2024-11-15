f1d222bb2bc83dbde8efcb2a878be2d39680ede3
Your Name <your.email@example.com>
2024-11-15 18:43:19 +0400

    mechvidmete

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        int[] store ={1, 2, 3, 4, 5};
-
-        int MinStore = 0;
-        int Min = store[4];
-        int MaxStore = 0;
-        int Max = 0;
-
-        for(int i = 0; i <= store.length; i++){
-            if(i==store.length){
-                System.out.println(MinStore + " magaziashi yvelaze iafia da girs " + Min);
-                System.out.println(MaxStore + " magaziashi yvelaze dzviria da girs " + Max);
-                break;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0 && i % 7 == 0) {
+                System.out.println("HEYHOW ");
             }
-            if(Max < store[i]){
-                Max = store[i];
-                MaxStore = i;
-            }
-            if(Min > store[i]){
-                Min = store[i];
-                MinStore = i;
+            else {
+                if (i % 5 == 0) {
+                    System.out.println("HEY ");
+                }
+                else {
+                    if (i % 7 == 0) {
+                        System.out.println("HOW ");
+                    }
+                    else {
+                        System.out.println(i + " ");
+                    }
+                }
             }
         }
-
     }
 }
